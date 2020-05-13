636200b51c9998c6c96d2b33e242184e62603a43
arseniyepti <ars-us@yandex.ru>
2020-05-13 15:16:35 +0300

    final2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -30,7 +30,7 @@ const store = createStore(
   compose(applyMiddleware(thunk), devtoolMiddleware)
 );
 
-render(
+ReactDOM.render(
   <Provider store={store}>
     <Normalize />
     <GlobalStyle />

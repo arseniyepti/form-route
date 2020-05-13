986092034a75f1597c781af0a5d7c7ae0d2951b6
arseniyepti <ars-us@yandex.ru>
2020-05-13 16:22:24 +0300

    final6

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,15 +1,156 @@
-import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Button, Input } from "antd";
+import "antd/dist/antd.css";
+import { Field, Formik } from "formik";
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import * as actions from "../actions/actions";
+import { validationSchemaAuthForm } from "../heplers/yupValidation.js";
+import AuthMessage from "./AuthMessage";
 
-class Authorization extends Component {
+const mapStateToProps = (state) => {
+  const { authState } = state;
+  return {
+    isLogged: authState.isLogged,
+    loading: authState.UIState.loading,
+    authorization: authState.UIState.authorization,
+  };
+};
+
+const actionCreators = {
+  setAuthState: actions.setAuthState,
+  AuthBtnLoading: actions.AuthBtnLoading,
+};
+
+class Authorization extends React.Component {
   render() {
+    const { loading, authorization, history } = this.props;
     return (
-      <div>
-        AAAAAAAAAAAAAA
-        <Link to="/form-route/login"> fsdgg</Link>
-      </div>
+      <Formik
+        initialValues={{
+          password: "",
+          email: "",
+        }}
+        validationSchema={validationSchemaAuthForm}
+        onSubmit={({ email, password }, { resetForm }) => {
+          const { setAuthState, AuthBtnLoading } = this.props;
+          AuthBtnLoading({ loading: true });
+          setAuthState(history, { email, password });
+          if (authorization && loading) {
+            resetForm();
+          }
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          setFieldTouched,
+          handleSubmit,
+        }) => (
+          <Section>
+            <StyledForm onSubmit={handleSubmit}>
+              <Label>
+                Email<SymSpan>*</SymSpan>
+                <Field
+                  onChange={(event) => {
+                    setFieldTouched("email");
+                    handleChange(event);
+                  }}
+                  value={values.email}
+                  name="email"
+                  id="email"
+                  type="email"
+                  component={StyledInput}
+                />
+              </Label>
+              {(touched.email && errors.email) || <div>&nbsp;</div>}
+              <Label>
+                Пароль<SymSpan>*</SymSpan>
+                <Field
+                  onChange={(event) => {
+                    setFieldTouched("password");
+                    handleChange(event);
+                  }}
+                  value={values.password}
+                  name="password"
+                  visibilityToggle
+                  id="password"
+                  type="password"
+                  component={StyledInputPassword}
+                />
+              </Label>
+              {(touched.password && errors.password) || <div>&nbsp;</div>}
+              <StyledButton loading={loading} htmlType="submit">
+                Войти
+              </StyledButton>
+              <AuthMessage authorization={authorization} />
+              <StyledLink to="/form-route/signup">Регистрация</StyledLink>
+            </StyledForm>
+          </Section>
+        )}
+      </Formik>
     );
   }
 }
 
-export default Authorization;
+const ConnectedAuthorization = connect(
+  mapStateToProps,
+  actionCreators
+)(Authorization);
+
+export default ConnectedAuthorization;
+
+const Section = styled.section`
+  padding: 30px;
+  width: 500px;
+  background-color: rgba(0, 33, 78, 0.12);
+  border-radius: 10px;
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: 10px;
+  margin-bottom: 5px;
+  width: 100px;
+`;
+
+const StyledInput = styled(Input)`
+  width: 300px;
+  display: flex;
+  align-self: flex-start;
+`;
+
+const StyledInputPassword = styled(Input.Password)`
+  width: 300px;
+  display: flex;
+  align-self: center;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  height: 220px;
+  flex-flow: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin-top: 10px;
+`;
+
+const SymSpan = styled.span`
+  font-size: 25px;
+  color: red;
+  margin-right: auto;
+  margin-left: 2px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+  align-items: center;
+`;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,13 +25,13 @@ class Main extends Component {
 
   render() {
     const { isLogged, user } = this.props;
-    // if (isLogged === false) {
-    //   return <Redirect to="/form-route/login" />;
-    // }
+    if (isLogged === false) {
+      return <Redirect to="/form-route/login" />;
+    }
     return (
       <Wrapper>
         <Name>{` Привет ${user}`}</Name>
-        <StyledLink onClick={this.handleLogOut} to="/form-route/">
+        <StyledLink onClick={this.handleLogOut} to="/form-route/login">
           Выход
         </StyledLink>
       </Wrapper>

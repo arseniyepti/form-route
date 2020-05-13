c21fd8ab757549f628cbc98fea7b796b6e88245f
arseniyepti <ars-us@yandex.ru>
2020-05-13 15:50:11 +0300

    final6

--- src/heplers/axiosInstance.js
+++ src/heplers/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://conduit.productionready.io/api",
-  // withCredentials: true,
+  withCredentials: true,
 });
 export default axiosInstance;

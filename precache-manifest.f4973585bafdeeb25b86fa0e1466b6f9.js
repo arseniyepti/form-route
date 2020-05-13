1b57b9ad43fa13c252627ee3b149b9d5c0c157db
arseniyepti <ars-us@yandex.ru>
2020-05-13 12:56:28 +0300

    Updates

---
+++ precache-manifest.f4973585bafdeeb25b86fa0e1466b6f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fd0a13a70ae9a7b8f198c58ff3245c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d081f9ddd5f47b82c28",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "4d081f9ddd5f47b82c28",
+    "url": "/static/js/2.3459cd61.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.3459cd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f63a8c4cab6a3af426",
+    "url": "/static/js/main.b2ec94dd.chunk.js"
+  },
+  {
+    "revision": "b26a15d16dc944c00a9f",
+    "url": "/static/js/runtime-main.6ce95106.js"
+  }
+]);

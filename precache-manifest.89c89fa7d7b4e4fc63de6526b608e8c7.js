c13cb30f3fbad0a52e9ad006add7528099b87f5b
arseniyepti <ars-us@yandex.ru>
2020-05-28 16:51:24 +0300

    Updates

--- precache-manifest.89c89fa7d7b4e4fc63de6526b608e8c7.js
+++ precache-manifest.89c89fa7d7b4e4fc63de6526b608e8c7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71935b430189b440acea8681835d8a40",
+    "revision": "d246b56f25deab683ee4a4b2246d0854",
     "url": "/form-route/index.html"
   },
   {
-    "revision": "734144deb5518a1238e8",
+    "revision": "c5b773d5a3e737fba87a",
     "url": "/form-route/static/css/2.35fcc229.chunk.css"
   },
   {
-    "revision": "734144deb5518a1238e8",
-    "url": "/form-route/static/js/2.1d84e4e1.chunk.js"
+    "revision": "c5b773d5a3e737fba87a",
+    "url": "/form-route/static/js/2.ee2c59ab.chunk.js"
   },
   {
     "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
-    "url": "/form-route/static/js/2.1d84e4e1.chunk.js.LICENSE.txt"
+    "url": "/form-route/static/js/2.ee2c59ab.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "45edb7a28dfff94287f6",
-    "url": "/form-route/static/js/main.6fe04d7e.chunk.js"
+    "revision": "046209fab2c3708f9554",
+    "url": "/form-route/static/js/main.3d48ea44.chunk.js"
   },
   {
     "revision": "b3b41b51995b7ef0f441",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/form-route/precache-manifest.a836b2791e778fca4d9e7884200f3694.js"
+  "/form-route/precache-manifest.89c89fa7d7b4e4fc63de6526b608e8c7.js"
 );
 
 self.addEventListener('message', (event) => {
